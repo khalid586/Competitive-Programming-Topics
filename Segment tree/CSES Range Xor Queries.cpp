57a5e456ec350b8b186fea91ee8da22e5d90c86f
khalid <khalidcomilla58@gmail.com>
2022-12-26 13:57:03 +0600

    third commit xor query

---
+++ Segment tree/CSES Range Xor Queries.cpp
@@ -0,0 +1,72 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define rep(i,a,b) for(int i = a ; i < b ; i++)
+#define endl '\n'
+ 
+void IO()
+{
+     std::ios::sync_with_stdio(false);cin.tie(NULL); 
+     #ifndef ONLINE_JUDGE
+          freopen( "in.txt" , "r" ,stdin);
+     #endif // ONLINE_JUDGE
+}
+ 
+const int N = 2e5 + 7;
+const int M = 4 * N + 7;
+int a[N];
+int minimum[M];
+ 
+void build(int at , int L , int R)
+{
+	if(L == R)
+	{
+		minimum[at] = a[L];
+		return ;
+	}
+ 
+	int mid = (L + R) / 2;
+	build(at * 2, L , mid);
+	build(at * 2 + 1 , mid + 1 , R);
+ 
+	minimum[at] = minimum[at * 2] ^ minimum[at * 2 + 1];
+}
+void update(int at , int L , int R , int pos , int u)
+{
+	if(L == R){
+		minimum[at] = u; return;
+	}
+	int mid = (L + R) / 2;
+	if(pos <= mid) update(at * 2 , L , mid , pos , u);
+	else		   update(at * 2 + 1 , mid + 1 , R , pos , u);
+ 
+	minimum[at] = min ( minimum[at * 2] , minimum[at * 2 + 1]);
+}
+ 
+int query(int at , int L , int R , int l , int r)
+{
+	if(L > r   or  R < l)	return 0;
+	if(L >= l and R <= r) 	return minimum[at];
+ 
+	int mid  = (L + R) / 2;
+	int left  = query(at * 2 , L , mid , l , r);
+	int right = query(at * 2 + 1 , mid + 1 , R , l , r);
+ 
+	return left ^ right;
+}
+ 
+int main()
+{
+	IO();
+	int n , q; 
+ 	cin >> n >> q;
+ 
+ 	rep(i,1,n+1) cin >> a[i];
+	build(1,1,n);
+ 
+	rep(i,0,q){
+		int l , r; cin >> l >> r; cout << query(1,1,n,l,r) << endl;
+		
+	}
+	return 0;
+}
