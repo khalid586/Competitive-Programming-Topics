f6a4e7c66153efdefeea528ab323a57da9c79d0a
khalid <khalidcomilla58@gmail.com>
2023-01-02 23:19:01 +0600

    basic math updated

---
+++ Basic Math/CodeForces 1370A Maximum GCD.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long int
+#define rep(i,a,b) for(int i = a ; i < b ; i++)
+#define endl "\n"
+
+void IO(){
+     #ifndef ONLINE_JUDGE
+          freopen("in.txt",  "r", stdin );
+     //     freopen("out.txt", "w", stdout);
+    //      freopen("Error.txt", "w", stderr);
+     #endif // ONLINE_JUDGE
+}
+
+
+const int N = 5e5 + 500;
+const int MX = 1e6;
+
+void solve(int t){
+     int n;
+     cin >> n;
+     cout << n/2 << endl;
+}
+
+int32_t main()
+{
+     IO();
+     std::ios::sync_with_stdio(false);cin.tie(NULL);
+
+     int t = 1;
+     cin >> t;
+
+     rep(i,1,t+1){
+          solve(i);
+     }
+     return 0;
+}
+
+
+
+
