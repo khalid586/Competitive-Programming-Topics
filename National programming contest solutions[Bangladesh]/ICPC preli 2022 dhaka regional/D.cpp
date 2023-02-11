63f35b084e41766ce833d61471cd620352332957
khalid <khalidcomilla58@gmail.com>
2023-02-11 21:10:50 +0600

    icpc preli '22 problem D

---
+++ National programming contest solutions[Bangladesh]/ICPC preli 2022 dhaka regional/D.cpp
@@ -0,0 +1,105 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+
+typedef long long int     ll;
+typedef vector<ll>      vll ;
+#define in(x)           cin>>x;
+#define khalid          std::ios::sync_with_stdio(false);cin.tie(NULL);
+#define out(x)          cout<<x<<"\n";
+#define out2(x)         cout<<x;
+#define newline         cout<<"\n";
+#define pb              push_back
+#define asc_sort(v)     sort(v.begin(), v.end())
+#define dsc_sort(v)     sort(v.begin(), v.end(),gt())
+#define rep(i,a,b)      for(ll i = a; i <  b;++i)
+#define rev(i,a,b)      for(ll j = a; j >= b;--j)   
+#define sum(v)          accumulate(v.begin(),v.end(),0ll)
+#define lld             long double
+#define ull             unsigned long long 
+#define all(s)          s.begin(),s.end()
+#define endl            "\n"
+#define lcm(a,b)        (a*b)/(__gcd(a,b))
+#define lcm3(a,b,c)     (lcm(a,b)*c)/(__gcd(c,lcm(a,b)))
+
+//----------------------------------------------------debugger-------------------------------------------------------------// |
+void _print(ll t) {cerr << t <<",";}                                                                                                
+void _print(int t) {cerr << t<<",";}
+void _print(string t) {cerr << t<<",";}
+void _print(char t) {cerr << t<<",";}
+void _print(lld t) {cerr << t<<",";}
+void _print(double t) {cerr << t<<",";}
+void _print(ull t) {cerr << t<<",";}
+ 
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.first); cerr << ","; _print(p.second); cerr << "}\n";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]\n";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]\n";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]\n";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]\n";}
+//----------------------------------------------------debugger-------------------------------------------------------------//|
+template<class T>
+using indexed_set = tree<T,null_type,less<T>,rb_tree_tag,
+tree_order_statistics_node_update>;
+// *s.find_by_order returns the value stored at that position
+// s.order_of_key returns the position where the value is stored
+
+//Starting in the name of Allah(THE ALMIGHTY)
+void FastIO()
+{
+     khalid
+    #ifndef ONLINE_JUDGE
+     freopen("in.txt","r", stdin);
+  //   freopen("out.txt", "w", stdout);
+    // freopen("Error.txt", "w", stderr);
+
+    #endif // ONLINE_JUDGE      
+}
+
+#define int long long int
+
+void solve(int t){
+     int a[3],k,sm = 0;
+     rep(i,0,3) cin >> a[i] , sm += a[i];
+     cin >> k;
+     sort(a,a+3);
+
+     cout << "Case " << t << ": " ;
+
+     if(a[0] == a[1] and a[2] == a[1])              { cout << "Peaceful\n";  return;}
+     if(sm % 3 or a[1] < k)                         { cout << "Fight\n";     return;}
+
+     int avg = sm / 3 , cnt = 0;
+     
+     rep(i,0,3){
+          if(abs(a[i] - avg) % k){
+               cout << "Fight\n"; return;
+          }
+          
+     }
+
+     cout << "Peaceful\n"; 
+}
+
+int32_t main()
+{
+     
+    // FastIO();
+     ll test_case = 1;
+     cin >> test_case;
+
+     rep(i,1,test_case + 1){
+          solve(i);
+     }
+
+
+     return 0;
+
+
+}
+
