d7244fbbad4c5d2a89e3f0cfce556d8422920248
khalid <khalidcomilla58@gmail.com>
2023-03-28 01:12:54 +0600

    Dijkstra's algo (solves CF 20C problem)

---
+++ Graph theory/Concepts/dijkstra.cpp
@@ -0,0 +1,72 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+#define int long long 
+const int N = 1e5 + 5;
+const int INF = 1e12+10;
+
+// solution of CF 20C dijkstra?
+vector< pair<int,int> > graph[N];
+vector<int> dist(N,INF);
+vector<int> path(N,0);
+void dijkstra()
+{
+	set<pair<int,int>> q;
+	q.insert({0,1}); 
+	dist[1] = 0;
+
+		while(!q.empty()){
+				int curr_dist=(*q.begin()).first , curr_edge = (*q.begin()).second; 
+				q.erase(*q.begin());
+				for(auto child:graph[curr_edge]){
+							int child_edge = child.second , child_dist = child.first;
+							
+							if( curr_dist + child_dist < dist[child_edge]){
+								dist[child_edge] = curr_dist + child_dist;
+								q.insert({dist[child_edge],child_edge});
+								path[child_edge] = curr_edge;	
+							}
+				}		
+	  }
+}
+
+void print(int node){
+	if(node == 1){
+		cout << node << ' '; return;
+	} 
+	print(path[node]);
+
+	cout << node << ' ';
+}
+
+int32_t main()
+{
+	     #ifndef ONLINE_JUDGE
+          freopen("in.txt","r", stdin);
+      //   freopen("out.txt", "w", stdout);
+          freopen("Error.txt", "w", stderr);
+     #endif // ONLINE_JUDGE
+
+	int node,edges; cin >> node  >> edges;
+	
+	for(int i=0;i<edges;i++)
+	{
+		int x,y,z; cin >> x >> y >> z;
+		if( x == y) continue;
+		graph[x].push_back({z,y}); graph[y].push_back({z,x});
+	}
+
+	vector<int> route;
+	dijkstra();
+
+	for (int i = 1; i <= node; ++i)
+	{
+		cout << dist[i] << ' ';
+	}
+	// if(path[node] == 0) { cout << -1 << '\n'; return 0;}
+	// print(node); // can print it by storing values in vector and then reversing it
+		
+	return 0;
+		
+}
