28383136c85f91e24d50658a9b25dd2b25a37052
khalid <khalidcomilla58@gmail.com>
2023-10-03 00:08:14 +0600

    code updated

--- Sorting and Searching/CSES apartments.cpp
+++ Sorting and Searching/CSES apartments.cpp
@@ -1,24 +1,13 @@
 #include <bits/stdc++.h>
-#include <ext/pb_ds/assoc_container.hpp>
-using namespace __gnu_pbds;
 using namespace std;
 
 #define rep(i,a,b) for(int i = a ; i < b ; i++)
-#define endl '\n'
-void IO()
-{
-     #ifndef ONLINE_JUDGE
-          freopen( "in.txt" , "r" ,stdin);
-      //   freopen("out.txt", "w", stdout);
-      //    freopen("Error.txt","w",stderr);
-     #endif // ONLINE_JUDGE
-}
- #define int long long int
+#define int long long int
 #define all(x) x.begin(),x.end()
 
 const int N = 2e5 + 7;
 
-void solve(int t){
+void solve(){
     int n,m,k; cin >> n >> m >> k;
 
     int client[n] ,apartment[m];
@@ -39,14 +28,9 @@ void solve(int t){
 int32_t main()
 {
      std::ios::sync_with_stdio(false);cin.tie(NULL); 
-     IO();
-     int t = 1;
-   //  cin >> t;
-
-     rep(i,1,t+1){
-          solve(i);
-     } 
+     
+     solve();
    
-   return 0;
+     return 0;
 }
 
