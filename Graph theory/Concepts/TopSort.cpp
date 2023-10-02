c0111cbd26aac6385771454cb3d84186544d4588
Khalid Abdullah <khalidcomilla58@gmail.com>
2023-10-02 20:33:43 +0600

    Updated TopSort.cpp

--- Graph theory/Concepts/TopSort.cpp
+++ Graph theory/Concepts/TopSort.cpp
@@ -3,13 +3,7 @@ using namespace std;
 
 #define rep(i,a,b) for(int i = a ; i < b ; i++)
 
-void IO()
-{
-     freopen( "in.txt" , "r" ,stdin);
-}
-
-const int N = 109;
-
+const int N = 100007;
 std::vector<int> graph[N];
 bool vis[N];
 
@@ -17,16 +11,15 @@ stack<int> ans;
 
 void dfs(int source){
      vis[source] = 1;
-
+     
      for(auto child:graph[source]){
           if(!vis[child]) dfs(child);
      }
-
      ans.push(source); // when all the childs of a node have been visited then we will push it into the stack
 }
 
 void solve(int t){
-     int n,m; cin >> n >> m;
+     int node,edge; cin >> node >> edge;
 
      rep(i,0,m){
           int u , v;
@@ -34,16 +27,18 @@ void solve(int t){
           graph[u].push_back(v); // directed graph
      }
 
-     rep(i,1,n+1) if(!vis[i]) dfs(i);
+     rep(i,1,node+1) 
+          if(!vis[i]) 
+               dfs(i);
      
      while(!ans.empty()){
           cout << ans.top() << ' '; ans.pop();
      }
+     cout << '\n';
 }
 
 int32_t main()
-{
-     IO();
+{     
      int t = 1; 
    //  cin >> t; 
 
